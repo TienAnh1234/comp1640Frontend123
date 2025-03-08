6dc88c094c5ff3e3d7a3e1cc5d6361bc9d60d407
TienAnh1234 <139440162+TienAnh1234@users.noreply.github.com>
2025-03-09 04:08:52 +0700

    Crud Major
    xem thêm sửa xóa major trong admin

---
+++ src/app/services/major.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Major } from '../common/major';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MajorService {
+
+
+  private majorUrl = 'http://localhost:8080/api/major';
+
+
+  constructor(private HttpClient: HttpClient) { }
+
+
+  saveMajor(major: Major): Observable<any> {
+    const saveMajor = `${this.majorUrl}/save_major`;
+    return this.HttpClient.post(saveMajor, major);
+  }
+
+  getMajorList(): Observable<any> {
+    return this.HttpClient.get<any>(this.majorUrl);
+  }
+
+
+  deleteMajor(id: number): Observable<void> {
+    return this.HttpClient.delete<void>(`${this.majorUrl}/delete/${id}`);
+  }
+
+  updateMajor(major: Major, majorId: number):Observable<any>{
+    return this.HttpClient.put(`${this.majorUrl}/update/${majorId}`, major);
+  }
+
+
+
+}
